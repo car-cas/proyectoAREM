ede87ca1e20f337ee9a4b475f622e2dfea2415f1
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2018-09-01 16:11:11 -0500

    avance servidor web

--- src/main/java/edu/escuelaing/arem/httpServer/HttpServer.java
+++ src/main/java/edu/escuelaing/arem/httpServer/HttpServer.java
@@ -5,10 +5,15 @@ import java.io.*;
 
 /**
  * @author Carlos Andrés Castaneda Lozano
- *
+ * 
  */
 
 public class HttpServer {
+    
+    /**
+     * @param args
+     * @throws IOException 
+     */
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
@@ -18,38 +23,55 @@ public class HttpServer {
             System.exit(1);
         }
         Socket clientSocket = null;
-        try {
-            System.out.println("Listo para recibir ...");
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("Received: " + inputLine);
-            if (!in.ready()) {
-                break;
+        while (true) {
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            String resultado;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Received: " + inputLine);
+                try {
+                    inputLine = inputLine.split(" ")[1];
+                    if (inputLine.endsWith(".html")) {
+                        File pagina = new File("./" + inputLine);
+                        resultado = " ";
+                        try {
+                            FileReader fReader = new FileReader(pagina);
+                            BufferedReader bReader = new BufferedReader(fReader);
+                            String line;
+                            while ((line = bReader.readLine()) != null) {
+                                resultado += line;
+                            }
+                            bReader.close();
+                        }catch (IOException ex) {
+                            System.err.println("Error en la lectura del Buffer");
+                            ex.printStackTrace();
+                        }
+                        if (!in.ready()) {
+                            break;
+                        }
+                        
+                        outputLine = "HTTP/1.1 200 OK\r\n"
+                                + "Content-Type: text/html\r\n"
+                                + "\r\n"
+                                + resultado
+                                + inputLine;
+                        out.println(outputLine);
+                    }
+
+                }catch(java.lang.ArrayIndexOutOfBoundsException e){
+                    
+                }
+                
             }
         }
-        outputLine = "HTTP/1.1 200 OK\r\n"
-                + "Content-Type: text/html\r\n"
-                + "\r\n"
-                + "<!DOCTYPE html>"
-                + "<html>"
-                + "<head>"
-                + "<meta charset=\"UTF-8\">"
-                + "<title>Title of the document</title>\n"
-                + "</head>" + "<body>"
-                + "My Web Site"
-                + "</body>"
-                + "</html>" + inputLine;
-        out.println(outputLine);
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
+
     }
 }
